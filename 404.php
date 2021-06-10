efef46fb60a2bcac25bb66f4cd3e90b66d8c0774
RedBoi/SilverStudios <adojoshua68@gmail.com>
2021-06-10 18:07:19 +0300

    html - php

---
+++ 404.php
@@ -0,0 +1,46 @@
+<!DOCTYPE php>
+<php lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="icon" href="favicon.ico" type="image/png">
+	<title>404 | Lolipop: Offline</title>
+	<meta name="description" content="Lollipop Online's Page but it's a 404 error">
+	<meta name="theme-color" content="#fc0362">
+	<link rel="stylesheet" type="text/css" href="/pages/css/modern-normalize.css">
+	<link rel="stylesheet" type="text/css" href="/pages/css/global.css">
+</head>
+<body>
+
+<header>
+	<a href="/">
+		<h1 style="margin:0"><img id="logo" src="/pages/img/logo.png" alt="Lolipop: Offline"/></h1>
+	</a>
+	<nav id="headbuttons">
+		<a href="https://discord.gg/hvfdrxVnyr" class="button_regular">Updates</a>
+		<a href="/faq.php" class="button_regular">FAQ</a>
+		<a href="/index.php" class="button_bold">Downloads</a>
+		<a href="/features.php" class="button_bold">Features</a>
+	</nav>
+</header>
+
+<main>
+
+<h1 id="404-file-not-found">404: File Not Found</h1>
+
+<p>You got a four o’ four<br />
+You got a four o’ four<br />
+When the path looks rough ahead<br />
+And you’re miles and miles<br />
+From your asset bed<br />
+You just remember what this error said<br />
+Boy, you got a four o’ four<br />
+Yeah, you got a four o’ four</p>
+
+<p><a href="/index.php">Click here for the downloads homepage</a><br />
+<a href="/features.php">Click here for the Features page</a></p>
+<a href="/faq.php">Click here for the FAQ page</a></p>
+
+
+</main>
+
+</body></php>
