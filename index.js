31b5d1f172384aa14358ca24c008ae3e2b982643
SnowFLG64 <70915454+SnowFLG64@users.noreply.github.com>
2022-12-24 03:03:31 +0000

    secret development

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import essential libraries 
+const express = require('express'); 
+const app = express(); 
+const path = require('path'); 
+const router = express.Router(); 
+// Setup essential routes 
+router.get('/', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/index.html')); 
+    //__dirname : It will resolve to your project folder. 
+}); 
+router.get('/about', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/about.html')); 
+}); 
+router.get('/sitemap', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/sitemap.html')); 
+}); 
+//add the router 
+app.use('/', router); 
+app.listen(process.env.port || 3000); 
+console.log('Running at Port 3000'); 
