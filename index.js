2201fd4ee4ce619aaf5d86abc26463307fa78afb
SnowFLG64 <70915454+SnowFLG64@users.noreply.github.com>
2023-01-12 14:52:29 +0000

    holy s*it

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-// Add Path to app.js
-require('./app')
-
-    // What AM I DOING WITH MY LIFE??? \\ 
-   // File Made By Josh                 \\
-  //                                     \\
- //                                       \\
-//                                         \\
-//////////////////////\\\\\\\\\\\\\\\\\\\\\\\\
+// Import essential libraries 
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+// Setup essential routes 
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+  //__dirname : It will resolve to your project folder. 
+});
+router.get('public/features', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/features.html'));
+});
+router.get('public/faq', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/faq.html'));
+});
+//add the router 
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000'); 
